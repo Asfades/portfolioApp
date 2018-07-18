3c81ab508cfb5865c98004c285ea20c08d7a3cb1
asfades <serheysy@gmail.com>
2018-07-18 15:17:22 +0300

    test3

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,5 @@
 const routes = [
-  { path: '/', component: require('../components/Login/LoginPage') },
+  { path: '/portfolioApp/', component: require('../components/Login/LoginPage') },
   { path: '/mainpage', component: require('../components/Mainpage') },
   { path: '/works', component: require('../components/Works') },
   { path: '/blog', component: require('../components/Blog') },
